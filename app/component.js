5559b5205ddaeff1bcb01429e3c1f27a8cabe68b
Miroslav <mirchev1977@novihorizonti.eu>
2016-08-25 16:11:56 +0300

    created a component and index.js to later display basic Hello World statement

---
+++ app/component.js
@@ -0,0 +1,11 @@
+(function(){
+	"use strict";
+
+	module.exports = function () {
+	  var element = document.createElement('h1');
+
+	  element.innerHTML = 'Hello world';
+
+	  return element;
+	};
+}());
---
+++ app/index.js
@@ -0,0 +1,7 @@
+(function(){
+	"use strict";
+	
+	var component = require('./component');
+
+	document.body.appendChild(component());
+}());
