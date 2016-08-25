368c2dd6f1ec0e0c6b36285b76852617f86767f5
Miroslav <mirchev1977@novihorizonti.eu>
2016-08-25 16:57:36 +0300

    integrated webpack-validator

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@
 	const path = require('path');
 	const HtmlWebpackPlugin = require('html-webpack-plugin');
 	const merge = require('webpack-merge');
+	const validate = require('webpack-validator');
 
 	const PATHS = {
 	  app: path.join(__dirname, 'app'),
@@ -39,5 +40,5 @@
 	    config = merge(common, {});
 	}
 
-	module.exports = config;
+	module.exports = validate(config);
 }());
