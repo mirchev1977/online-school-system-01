2a47840a58644e679b8be42046769b25fc825e8b
Miroslav <mirchev1977@novihorizonti.eu>
2016-08-26 08:50:55 +0300

    purified unused css in build

--- app/component.js
+++ app/component.js
@@ -4,6 +4,7 @@
 	module.exports = function () {
 	  var element = document.createElement('h1');
 
+	  element.className = 'pure-button';
 	  element.innerHTML = 'Hello world!!! This is a nice option! Ha ha!';
 
 	  return element;
--- build/style.6bf4283be70971cbfc50.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1,3],[function(n,c){}]);
-//# sourceMappingURL=style.6bf4283be70971cbfc50.js.map
