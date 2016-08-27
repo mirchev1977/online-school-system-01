97d17902c05adcdef06e19e6610062e8b994e7e7
Miroslav <mirchev1977@novihorizonti.eu>
2016-08-27 17:07:35 +0300

    use ecma script 2015 in project

---
+++ app/ecmaComponent.es6
@@ -0,0 +1,11 @@
+(function(){
+	"use strict";
+
+	module.exports = function () {
+	  let element = document.createElement("h1");
+
+	  element.innerHTML = "I'm Ecma Script 2015.";
+
+	  return element;
+	};
+}());
--- app/index.js
+++ app/index.js
@@ -4,8 +4,10 @@
 	require("react");
 	
 	var component = require("./component");
+	var ecmaComponent = require("./ecmaComponent");
 	var message = "Hello from index.js";
 
 	document.body.appendChild(component());
+	document.body.appendChild(ecmaComponent());
 	console.log(message);
 }());
