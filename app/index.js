67c3e285ea040f6cb0a3da93ec63363a2115663f
Miroslav <mirchev1977@novihorizonti.eu>
2016-08-27 11:39:40 +0300

    added jshint linting options

--- app/index.js
+++ app/index.js
@@ -4,7 +4,8 @@
 	require("react");
 	
 	var component = require("./component");
+	var message = "Hello from index.js";
 
 	document.body.appendChild(component());
-	console.log("Hello from index.js");
+	console.log(message);
 }());
