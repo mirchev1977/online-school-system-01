d03c42d4a9fd158733c9f39e4c8f4f260a064524
Miroslav <mirchev1977@novihorizonti.eu>
2016-08-31 12:17:30 +0300

    ignore build in gitignore

--- build/style.44af0f90c66fd67e4712.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1,3],{0:function(n,o,c){c(32),n.exports=c(36)},32:function(n,o){},36:function(n,o){}});
-//# sourceMappingURL=style.44af0f90c66fd67e4712.js.map
--- build/style.6acbd4ac1a5d89b165e6.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1,3],{0:function(n,o,c){c(34),n.exports=c(38)},34:function(n,o){},38:function(n,o){}});
-//# sourceMappingURL=style.6acbd4ac1a5d89b165e6.js.map
