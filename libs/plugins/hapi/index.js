11530b3057fa0e1349cc65bb9af34c2a8c743b38
Miroslav <mirchev1977@novihorizonti.eu>
2016-08-31 15:32:13 +0300

    installed inert

---
+++ libs/plugins/hapi/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Hapi = require('hapi');
+// const Sqlite3 = require('sqlite3');
+
+const DB_FILE = __dirname + './oss.sqlite';
+
+exports.register = function(server, options, next){
+
+
+	const db = new Sqlite3.Database(DB_FILE);
+
+	server.bind({ db: db });
+
+	server.register([
+            require('inert')
+        ], (err) => {
+
+        if (err) {
+            return next(err);
+        }
+
+        server.route(require('./routes/routes'));
+
+        next();
+    });
+}
+
+exports.register.attributes = {
+	pkg: require('./package.json')
+}
